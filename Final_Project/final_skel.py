b4b8674a5b8b7c50191d039a26e1ccec379bb57b
Ashwin Chidambaram <ashwin.chidambaram@gmail.com>
2020-03-07 20:15:10 -0800

    changes

--- Final_Project/final_skel.py
+++ Final_Project/final_skel.py
@@ -24,7 +24,7 @@ class final_topo(Topo):
         h2 = self.addHost('h2', mac='00:00:00:00:00:02', ip='10.0.2.102/24', defaultRoute="h2-eth0")                 ## Host 2
         h3 = self.addHost('h3', mac='00:00:00:00:00:03', ip='10.0.3.103/24', defaultRoute="h3-eth0")                 ## Host 3
         h4 = self.addHost('server', mac='00:00:00:00:00:04', ip='10.0.1.104/24', defaultRoute="s1-eth0")                 ## Server
-        h5 = self.addHost('h4', mac='00:00:00:00:00:05', ip='128.114.50.10/24', defaultRoute="h4-eth0")              ## Untrusted Host [4]
+        h5 = self.addHost('h5', mac='00:00:00:00:00:05', ip='128.114.50.10/24', defaultRoute="h4-eth0")              ## Untrusted Host [4]
 
         #### Set up Switches ###################################################
         s1 = self.addSwitch('Floor_1')                                                                             ## Adds Floor 1 Switch
@@ -43,8 +43,8 @@ class final_topo(Topo):
         self.addLink(h1, s1, port1=0, port2=1)
         self.addLink(h2, s2, port1=0, port2=1)
         self.addLink(h3, s3, port1=0, port2=1)
-        self.addLink(h4, s5, port1=0, port2=1)
-        self.addLink(h5, s4, port1=0, port2=1)
+        self.addLink(h4, s4, port1=0, port2=1)
+        self.addLink(h5, s5, port1=0, port2=1)
 
         # Link to Core Switch
         self.addLink(s1, s4, port1=3, port2=2)
