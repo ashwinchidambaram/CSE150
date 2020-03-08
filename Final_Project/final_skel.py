81f42ee3ed9cb1a5538fd2ed735564aa3f5797a4
Ashwin Chidambaram <ashwin.chidambaram@gmail.com>
2020-03-07 20:06:38 -0800

    changes

--- Final_Project/final_skel.py
+++ Final_Project/final_skel.py
@@ -20,18 +20,18 @@ class final_topo(Topo):
     def build(self):
 
         #### Set up Hosts ######################################################
-        host1 = self.addHost('h1', mac='00:00:00:00:00:01', ip='10.0.1.101/24', defaultRoute="h1-eth0")                 ## Host 1
-        host2 = self.addHost('h2', mac='00:00:00:00:00:02', ip='10.0.2.102/24', defaultRoute="h2-eth0")                 ## Host 2
-        host3 = self.addHost('h3', mac='00:00:00:00:00:03', ip='10.0.3.103/24', defaultRoute="h3-eth0")                 ## Host 3
-        host4 = self.addHost('S1', mac='00:00:00:00:00:04', ip='10.0.1.104/24', defaultRoute="s1-eth0")                 ## Server
-        host5 = self.addHost('h4', mac='00:00:00:00:00:05', ip='128.114.50.10/24', defaultRoute="h4-eth0")              ## Untrusted Host [4]
+        h1 = self.addHost('h1', mac='00:00:00:00:00:01', ip='10.0.1.101/24', defaultRoute="h1-eth0")                 ## Host 1
+        h2 = self.addHost('h2', mac='00:00:00:00:00:02', ip='10.0.2.102/24', defaultRoute="h2-eth0")                 ## Host 2
+        h3 = self.addHost('h3', mac='00:00:00:00:00:03', ip='10.0.3.103/24', defaultRoute="h3-eth0")                 ## Host 3
+        h4 = self.addHost('server', mac='00:00:00:00:00:04', ip='10.0.1.104/24', defaultRoute="s1-eth0")                 ## Server
+        h5 = self.addHost('h4', mac='00:00:00:00:00:05', ip='128.114.50.10/24', defaultRoute="h4-eth0")              ## Untrusted Host [4]
 
         #### Set up Switches ###################################################
-        switch1 = self.addSwitch('Floor_1')                                                                             ## Adds Floor 1 Switch
-        switch2 = self.addSwitch('Floor_2')                                                                             ## Adds Floor 2 Switch
-        switch3 = self.addSwitch('Floor_3')                                                                             ## Adds Floor 3 Switch
-        switch4 = self.addSwitch('Core')                                                                                ## Adds Core Switch
-        switch5 = self.addSwitch('Data_Center')                                                                         ## Adds Data Center Switch
+        s1 = self.addSwitch('Floor_1')                                                                             ## Adds Floor 1 Switch
+        s2 = self.addSwitch('Floor_2')                                                                             ## Adds Floor 2 Switch
+        s3 = self.addSwitch('Floor_3')                                                                             ## Adds Floor 3 Switch
+        s4 = self.addSwitch('Core')                                                                                ## Adds Core Switch
+        s5 = self.addSwitch('Data_Center')                                                                         ## Adds Data Center Switch
 
         #### Link Switches & Hosts #############################################
 
@@ -40,17 +40,17 @@ class final_topo(Topo):
         # connecting to.
 
         # Link the Hosts to their relevent switches
-        self.addLink(host1, switch1, port1=0, port2=1)
-        self.addLink(host2, switch2, port1=0, port2=1)
-        self.addLink(host3, switch3, port1=0, port2=1)
-        self.addLink(host4, switch5, port1=0, port2=1)
-        self.addLink(host5, switch4, port1=0, port2=1)
+        self.addLink(h1, s1, port1=0, port2=1)
+        self.addLink(h2, s2, port1=0, port2=1)
+        self.addLink(h3, s3, port1=0, port2=1)
+        self.addLink(h4, s5, port1=0, port2=1)
+        self.addLink(h5, s4, port1=0, port2=1)
 
         # Link to Core Switch
-        self.addLink(switch1, switch4, port1=3, port2=2)
-        self.addLink(switch2, switch4, port1=3, port2=3)
-        self.addLink(switch3, switch4, port1=3, port2=4)
-        self.addLink(switch5, switch4, port1=3, port2=5)
+        self.addLink(s1, s4, port1=3, port2=2)
+        self.addLink(s2, s4, port1=3, port2=3)
+        self.addLink(s3, s4, port1=3, port2=4)
+        self.addLink(s5, s4, port1=3, port2=5)
 
 def configure():
     topo = final_topo()
