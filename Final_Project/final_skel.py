1fc149e03e9852d5adfaba67e5fb2f747a4c0ef4
Ashwin Chidambaram <ashwin.chidambaram@gmail.com>
2020-03-08 15:40:16 -0700

    changes

--- Final_Project/final_skel.py
+++ Final_Project/final_skel.py
@@ -20,33 +20,33 @@ class final_topo(Topo):
     def build(self):
 
         #### Set up Hosts ######################################################
-        h1 = self.addHost('h1',mac='00:00:00:00:00:01',ip='10.0.1.101/24',defaultRoute="h1-eth0")
-        h2 = self.addHost('h2',mac='00:00:00:00:00:02',ip='10.0.2.102/24',defaultRoute="h2-eth0")
-        h3 = self.addHost('h3',mac='00:00:00:00:00:03',ip='10.0.3.103/24',defaultRoute="h3-eth0")
-        h4 = self.addHost('h4',mac='00:00:00:00:00:04',ip='128.114.50.10/24',defaultRoute="h4-eth0")
-        h5 = self.addHost('h5',mac='00:00:00:00:00:04',ip='10.0.4.104/24',defaultRoute="h5-eth0")
+        h1 = self.addHost('h1',mac='00:00:00:00:00:01',ip='10.0.1.101/24',defaultRoute="h1-eth0")               ## Host 1
+        h2 = self.addHost('h2',mac='00:00:00:00:00:02',ip='10.0.2.102/24',defaultRoute="h2-eth0")               ## Host 2
+        h3 = self.addHost('h3',mac='00:00:00:00:00:03',ip='10.0.3.103/24',defaultRoute="h3-eth0")               ## Host 3
+        h4 = self.addHost('h4',mac='00:00:00:00:00:04',ip='128.114.50.10/24',defaultRoute="h4-eth0")            ## Untrusted Host
+        h5 = self.addHost('h5',mac='00:00:00:00:00:04',ip='10.0.4.104/24',defaultRoute="h5-eth0")               ## Server
 
         #### Set up Switches ###################################################
-        s1 = self.addSwitch('s1')                                                                             ## Adds Floor 1 Switch
-        s2 = self.addSwitch('s2')                                                                             ## Adds Floor 2 Switch
-        s3 = self.addSwitch('s3')                                                                             ## Adds Floor 3 Switch
-        s4 = self.addSwitch('s4')                                                                                ## Adds Core Switch
-        s5 = self.addSwitch('s5')
+        s1 = self.addSwitch('s1')                                                                               ## Adds Floor 1 Switch
+        s2 = self.addSwitch('s2')                                                                               ## Adds Floor 2 Switch
+        s3 = self.addSwitch('s3')                                                                               ## Adds Floor 3 Switch
+        s4 = self.addSwitch('s4')                                                                               ## Adds Core Switch
+        s5 = self.addSwitch('s5')                                                                               ## Adds Data Center Switch
 
         #### Link Switches & Hosts #############################################
 
         # Link the Hosts to their relevent switches
-        self.addLink(s1, h1, port1=8, port2=0)
-        self.addLink(s2, h2, port1=8, port2=0)
-        self.addLink(s3, h3, port1=8, port2=0)
-        self.addLink(s4, h4, port1=8, port2=0)
-        self.addLink(s5, h5, port1=8, port2=0)
+        self.addLink(s1, h1, port1=8, port2=0)                                                                  ## Link Switch 1 & Host 1   -   [Floor Switch 1 <--> Host 1]
+        self.addLink(s2, h2, port1=8, port2=0)                                                                  ## Link Switch 2 & Host 2   -   [Floor Switch 2 <--> Host 2]
+        self.addLink(s3, h3, port1=8, port2=0)                                                                  ## Link Switch 3 & Host 3   -   [Floor Switch 3 <--> Host 3]
+        self.addLink(s4, h4, port1=8, port2=0)                                                                  ## Link Switch 4 & Host 4   -   [Core Switch <--> Untrusted Host]
+        self.addLink(s5, h5, port1=8, port2=0)                                                                  ## Link Switch 4 & Host 4   -   [Data Center Switch <--> Server]
 
         # Link to Core Switch
-        self.addLink(s1, s4, port1=3, port2=1)
-        self.addLink(s2, s4, port1=3, port2=2)
-        self.addLink(s3, s4, port1=3, port2=3)
-        self.addLink(s5, s4, port1=3, port2=4)
+        self.addLink(s1, s4, port1=3, port2=1)                                                                  ## Link Switch 1 & Switch 4 -   [Floor Switch 1 <--> Core Switch]
+        self.addLink(s2, s4, port1=3, port2=2)                                                                  ## Link Switch 2 & Switch 4 -   [Floor Switch 2 <--> Core Switch]
+        self.addLink(s3, s4, port1=3, port2=3)                                                                  ## Link Switch 3 & Switch 4 -   [Floor Switch 3 <--> Core Switch]
+        self.addLink(s5, s4, port1=3, port2=4)                                                                  ## Link Switch 5 & Switch 4 -   [Data Center Switch <--> Core Switch]
 
 def configure():
     topo = final_topo()
@@ -56,7 +56,6 @@ def configure():
     CLI(net)
 
     net.stop()
-
 
 if __name__ == '__main__':
     configure()
--- Final_Project/finalcontroller_skel.py
+++ Final_Project/finalcontroller_skel.py
@@ -1,3 +1,12 @@
+##################################################
+## Final Project - Implementing a Simple Router ##
+##################################################
+## Ashwin Chidambaram                           ##
+## 03/06/2020                                   ##
+##################################################
+## final_controller.py ###########################
+##################################################
+
 # Final Skeleton
 #
 # Hints/Reminders from Lab 4:
@@ -24,61 +33,188 @@ import pox.openflow.libopenflow_01 as of
 log = core.getLogger()
 
 class Final (object):
-  """
-  A Firewall object is created for each switch that connects.
-  A Connection object for that switch is passed to the __init__ function.
-  """
-  def __init__ (self, connection):
-    # Keep track of the connection to the switch so that we can
-    # send it messages!
-    self.connection = connection
-
-    # This binds our PacketIn event listener
-    connection.addListeners(self)
-
-  def do_final (self, packet, packet_in, port_on_switch, switch_id):
-    # This is where you'll put your code. The following modifications have
-    # been made from Lab 4:
-    #   - port_on_switch represents the port that the packet was received on.
-    #   - switch_id represents the id of the switch that received the packet
-    #      (for example, s1 would have switch_id == 1, s2 would have switch_id == 2, etc...)        # Variables
-
-    idle_timeout = 30                                                       # Time taken to timeout in seconds
-    hard_timeout = 60                                                       # Max time taken to timeout in seconds
-
-    msg = of.ofp_flow_mod()
-
-    # Match Packet
-    msg.match = of.ofp_match.from_packet(packet)
-
-    # Timeout timers
-    msg.idle_timeout = idle_timeout
-    msg.hard_timeout = hard_timeout
-
-    print "Hello, World!"
-
-    msg.data = packet_in
-    action = of.ofp_action_output(port = of.OFPP_FLOOD)
-    msg.actions.append(action)
-    self.connection.send(msg)
-
-  def _handle_PacketIn (self, event):
     """
-    Handles packet in messages from the switch.
+    A Firewall object is created for each switch that connects.
+    A Connection object for that switch is passed to the __init__ function.
     """
-    packet = event.parsed # This is the parsed packet data.
-    if not packet.parsed:
-      log.warning("Ignoring incomplete packet")
-      return
+    def __init__ (self, connection):
+        # Keep track of the connection to the switch so that we can
+        # send it messages!
+        self.connection = connection
 
-    packet_in = event.ofp # The actual ofp_packet_in message.
-    self.do_final(packet, packet_in, event.port, event.dpid)
+        # This binds our PacketIn event listener
+        connection.addListeners(self)
+
+    def do_final (self, packet, packet_in, port_on_switch, switch_id):
+        # This is where you'll put your code. The following modifications have
+        # been made from Lab 4:
+        #   - port_on_switch represents the port that the packet was received on.
+        #   - switch_id represents the id of the switch that received the packet
+        #      (for example, s1 would have switch_id == 1, s2 would have switch_id == 2, etc...)        # Variables
+
+        idle_timeout = 30                                                               ## Time taken to timeout in seconds
+        hard_timeout = 60                                                               ## Max time taken to timeout in seconds
+
+        msg = of.ofp_flow_mod()
+
+        # Match Packet
+        msg.match = of.ofp_match.from_packet(packet)
+
+        # Timeout timers
+        msg.idle_timeout = idle_timeout
+        msg.hard_timeout = hard_timeout
+
+        untrustedHost_IP = '128.114.50.10'
+
+        protocol_TCP = packet.find('tcp')
+    	protocol_ARP = packet.find('arp')
+    	protocol_ICMP = packet.find('icmp')
+        protocol_IPv4 = packet.find('ipv4')
+
+        #### IPv4 Check ########################################################
+        if protocol_IPv4 is not None:                                                   ## if IS IPv4
+
+            #### Switch 1 ######################################################
+            if switch_id is 1:
+
+                if port_on_switch is 8:                                                 ## Traffic coming in from Host 1 [h1]
+
+                    # Action to send to specified port
+                    action = of.ofp_action_output(port = 3)                             ## Send packet to port 3 of Core Switch [s4]
+                    msg.actions.append(action)
+
+                elif port_on_switch is 3:                                               ## Traffic coming in from Core Switch [s4]
+
+                    # Action to send to specified port
+                    action = of.ofp_action_output(port = 8)                             ## Send packet to port 8 of Host 1 [h1]
+                    msg.actions.append(action)
+
+                else:
+                    print 'Unexpected Event @ Switch_ID 1'
+
+            #### Switch 2 ######################################################
+            elif switch_id is 2:
+
+                if port_on_switch is 8:                                                 ## Traffic coming in from Host 2 [h2]
+
+                    # Action to send to specified port
+                    action = of.ofp_action_output(port = 3)                             ## Send packet to port 3 of Core Switch [s4]
+                    msg.actions.append(action)
+
+                elif port_on_switch is 3:                                               ## Traffic coming in from Core Switch [s4]
+
+                    # Action to send to specified port
+                    action = of.ofp_action_output(port = 8)                             ## Send packet to port 8 of Host 2 [h2]
+                    msg.actions.append(action)
+
+                else:
+                    print 'Unexpected Event @ Switch_ID 2'
+
+            #### Switch 3 ######################################################
+            elif switch_id is 3:
+
+                if port_on_switch is 8:                                                 ## Traffic coming in from Host 2 [h2]
+
+                    # Action to send to specified port
+                    action = of.ofp_action_output(port = 3)                             ## Send packet to port 3 of Core Switch [s4]
+                    msg.actions.append(action)
+
+                elif port_on_switch is 3:                                               ## Traffic coming in from Core Switch [s4]
+
+                    # Action to send to specified port
+                    action = of.ofp_action_output(port = 8)                             ## Send packet to port 8 of Host 2 [h2]
+                    msg.actions.append(action)
+
+                else:
+                    print 'Unexpected Event @ Switch_ID 3'
+
+            #### Switch 5 ######################################################
+            elif switch_id is 5:
+
+            #### Switch 4 ######################################################
+            elif switch_id is 4:
+
+        else:                                                                   # if NOT IPv4
+
+            action = of.ofp_action_output(port = of.OFPP_FLOOD)
+            self.connection.send(msg)
+
+
+
+
+        ### TCP Check ##########################################################
+        if protocol_TCP is not None:
+
+
+            # Check if IPv4
+            protocol_IPv4 = packet.find('ipv4')
+
+            if protocol_IPv4 is not None:
+                # Take in data packet
+                msg.data = packet_in
+
+                # Check if TCP type
+                msg.IP_PROTO = 6                                                ###-1-###
+
+                # Action to send to specified port                              ###-2-###
+                action = of.ofp_action_output(port = of.OFPP_FLOOD)
+                msg.actions.append(action)
+
+                # Send message to switch
+                self.connection.send(msg)
+
+            # If source/destination IP don't match condition, then just direct to switch
+            else:
+                # Send message to switch
+                self.connection.send(msg)
+
+        #### ARP Check #########################################################
+        elif protocol_ARP is not None:
+
+            # Take in data packet
+            msg.data = packet_in
+
+            # Check if ARP type
+            msg.match.ETH_TYPE = 0x0806                                          ###-1-###
+
+            # Action to send to specified port                                  ###-2-###
+            action = of.ofp_action_output(port = of.OFPP_FLOOD)
+            msg.actions.append(action)
+
+            # Send message to switch
+            self.connection.send(msg)
+
+        ########################################################################
+        else:
+		self.connection.send(msg)
+
+######
+
+
+    # RAW TEST SCRIPT###############
+    #msg.data = packet_in
+    #action = of.ofp_action_output(port = of.OFPP_FLOOD)
+    #msg.actions.append(action)
+    #self.connection.send(msg)
+
+    def _handle_PacketIn (self, event):
+        """
+        Handles packet in messages from the switch.
+        """
+        packet = event.parsed # This is the parsed packet data.
+        if not packet.parsed:
+          log.warning("Ignoring incomplete packet")
+          return
+
+        packet_in = event.ofp # The actual ofp_packet_in message.
+        self.do_final(packet, packet_in, event.port, event.dpid)
 
 def launch ():
-  """
-  Starts the component
-  """
-  def start_switch (event):
-    log.debug("Controlling %s" % (event.connection,))
-    Final(event.connection)
-  core.openflow.addListenerByName("ConnectionUp", start_switch)
+    """
+      Starts the component
+      """
+      def start_switch (event):
+          log.debug("Controlling %s" % (event.connection,))
+          Final(event.connection)
+
+      core.openflow.addListenerByName("ConnectionUp", start_switch)
