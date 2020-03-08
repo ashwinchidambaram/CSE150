8c6d6eb356c34a50e2526ff3bd32e411fbd3c568
Ashwin Chidambaram <ashwin.chidambaram@gmail.com>
2020-03-07 19:40:33 -0800

    changes

--- Final_Project/final_skel.py
+++ Final_Project/final_skel.py
@@ -1,3 +1,12 @@
+##################################################
+## Final Project - Implementing a Simple Router ##
+##################################################
+## Ashwin Chidambaram                           ##
+## 03/06/2020                                   ##
+##################################################
+## final.py ######################################
+##################################################
+
 #!/usr/bin/python
 
 from mininet.topo import Topo
@@ -10,52 +19,38 @@ from mininet.node import RemoteController
 class final_topo(Topo):
     def build(self):
 
-        # Set Up Topology Here
-        switch1 = self.addSwitch('Floor_1')                                     ## Adds Switch 1    [s1]
-        switch2 = self.addSwitch('Floor_2')                                     ## Adds Switch 2    [s2]
-        switch3 = self.addSwitch('Floor_3')                                     ## Adds Switch 3    [s3]
-        switch4 = self.addSwitch('Core')                                        ## Adds Switch 3    [s3]
-        switch5 = self.addSwitch('Data_Center')                                 ## Adds Switch 3    [s3]
+        #### Set up Hosts ######################################################
+        host1 = self.addHost('h1', mac='00:00:00:00:00:01', ip='10.0.1.101/24', defaultRoute="h1-eth0")                 ## Host 1
+        host2 = self.addHost('h2', mac='00:00:00:00:00:02', ip='10.0.2.102/24', defaultRoute="h2-eth0")                 ## Host 2
+        host3 = self.addHost('h3', mac='00:00:00:00:00:03', ip='10.0.3.103/24', defaultRoute="h3-eth0")                 ## Host 3
+        host4 = self.addHost('S1', mac='00:00:00:00:00:04', ip='10.0.1.104/24', defaultRoute="s1-eth0")                 ## Server
+        host5 = self.addHost('h4', mac='00:00:00:00:00:05', ip='128.114.50.10/24', defaultRoute="h4-eth0")              ## Untrusted Host [4]
 
-        host1 = self.addHost('h1', mac='00:00:00:00:00:01', ip='10.0.1.101/24', defaultRoute="h1-eth0")
-        self.addLink(host1, switch1)                                            ## Link             [h1] to [s1]
+        #### Set up Switches ###################################################
+        switch1 = self.addSwitch('Floor_1')                                                                             ## Adds Floor 1 Switch
+        switch2 = self.addSwitch('Floor_2')                                                                             ## Adds Floor 2 Switch
+        switch3 = self.addSwitch('Floor_3')                                                                             ## Adds Floor 3 Switch
+        switch4 = self.addSwitch('Core')                                                                                ## Adds Core Switch
+        switch5 = self.addSwitch('Data_Center')                                                                         ## Adds Data Center Switch
 
-        host2 = self.addHost('h2', mac='00:00:00:00:00:02', ip='10.0.1.102/24', defaultRoute="h2-eth0")
-        self.addLink(host2, switch2)                                            ## Link             [h1] to [s1]
+        #### Link Switches & Hosts #############################################
 
-        host3 = self.addHost('h3', mac='00:00:00:00:00:03', ip='10.0.1.103/24', defaultRoute="h3-eth0")
-        self.addLink(host3, switch3)                                            ## Link             [h1] to [s1]
+        # Connect Port 8 on the Switch to Port 0 on Host 1 and Port 9 on the Switch to Port 0 on
+        # Host 2. This is representing the physical port on the switch or host that you are
+        # connecting to.
 
-        self.addLink(switch1, switch4)                                            ## Link             [h1] to [s1]
-        self.addLink(switch2, switch4)                                            ## Link             [h1] to [s1]
-        self.addLink(switch3, switch4)                                            ## Link             [h1] to [s1]
+        # Link the Hosts to their relevent switches
+        self.addLink(host1, switch1, port1=1, port2=2)
+        self.addLink(host2, switch2, port1=1, port2=2)
+        self.addLink(host3, switch3, port1=1, port2=2)
+        self.addLink(host5, switch4, port1=1, port2=5)
+        self.addLink(host4, switch5, port1=1, port2=5)
 
-        host4 = self.addHost('h4', mac='00:00:00:00:00:05', ip='128.114.50.10/24', defaultRoute="h4-eth1")
-        self.addLink(host4, switch4)                                            ## Link             [h1] to [s1]
-
-        self.addLink(switch4, switch5)                                            ## Link             [h1] to [s1]
-
-        host5 = self.addHost('h5', mac='00:00:00:00:00:04', ip='10.0.1.104/24', defaultRoute="h5-eth0")
-        self.addLink(host5, switch5)                                            ## Link             [h1] to [s1]
-
-
-    # Examples!
-    # Create a host with a default route of the ethernet interface. You'll need to set the
-    # default gateway like this for every host you make on this assignment to make sure all
-    # packets are sent out that port. Make sure to change the h# in the defaultRoute area
-    # and the MAC address when you add more hosts!
-
-    #h1 = self.addHost('h1',mac='00:00:00:00:00:01',ip='1.1.1.1/24', defaultRoute="h1-eth0")
-    #h2 = self.addHost('h2',mac='00:00:00:00:00:02',ip='2.2.2.2/24', defaultRoute="h2-eth0")
-
-    # Create a switch. No changes here from Lab 1.
-    #s1 = self.addSwitch('s1')
-
-    # Connect Port 8 on the Switch to Port 0 on Host 1 and Port 9 on the Switch to Port 0 on
-    # Host 2. This is representing the physical port on the switch or host that you are
-    # connecting to.
-    #self.addLink(s1,h1, port1=8, port2=0)
-    #self.addLink(s1,h2, port1=9, port2=0)
+        # Link to Core Switch
+        self.addLink(switch1, switch4, port1=3, port2=1)
+        self.addLink(switch2, switch4, port1=3, port2=2)
+        self.addLink(switch3, switch4, port1=3, port2=3)
+        self.addLink(switch5, switch4, port1=3, port2=4)
 
 def configure():
     topo = final_topo()
